2dc92cb93fb4a9a4cd4bae65cc412cea8d0af8e5
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-10 15:49:09 +0300

    crud işemleri

---
+++ CasgemPortfolio/Controllers/ServiceController.cs
@@ -0,0 +1,50 @@
+﻿using CasgemPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CasgemPortfolio.Models.Entities;
+
+
+namespace CasgemPortfolio.Controllers
+{
+    public class ServiceController : Controller
+    {           
+      
+        // GET: Service
+        CasgemPortfolioEntities db = new CasgemPortfolioEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblService.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult Service()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddService(TblService p)
+        {
+            db.TblService.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("İndex");
+        }
+
+        public ActionResult DeleteService(int id)
+        {
+            var value = db.TblService.Find(id);
+            db.TblService.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UpdateService(int id)
+        {
+            var value = db.TblService.Find(id);
+            return View(value);
+        }
+
+    }
+    
+}
