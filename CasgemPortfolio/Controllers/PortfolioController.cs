4fe9d0b8c9a19bb5439e394dc68eab24dd443afe
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-11 10:42:25 +0300

    Portfolio

---
+++ CasgemPortfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CasgemPortfolio.Models.Entities;
+
+namespace CasgemPortfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        CasgemPortfolioEntities db = new CasgemPortfolioEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar() 
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFeature()
+        {
+            ViewBag.featureTitle = db.TblFeature.Select(x =>
+               x.FeatureTitle).FirstOrDefault();
+            ViewBag.featureDescription = db.TblFeature.Select(x =>
+            x.FeatureDescription).FirstOrDefault();
+            ViewBag.featureImageURL = db.TblFeature.Select(x =>
+            x.FeatureImageURL).FirstOrDefault();
+            return PartialView();
+        }
+    }
+}
